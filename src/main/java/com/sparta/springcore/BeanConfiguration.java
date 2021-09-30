d49d9c3b9a3abf4b47e3f4bda1f40d9f31b86b7d
woodimora <woodimora@gmail.com>
2021-09-30 17:40:02 +0900

    1주차 과제완료

---
+++ src/main/java/com/sparta/springcore/BeanConfiguration.java
@@ -0,0 +1,16 @@
+package com.sparta.springcore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+//    @Bean
+//    public ProductRepository productRepository() {
+//        String dbUrl = "jdbc:h2:mem:springcoredb";
+//        String dbId = "sa";
+//        String dbPassword = "";
+//
+//        return new ProductRepository(dbUrl, dbId, dbPassword);
+//    }
+}
---
+++ src/main/java/com/sparta/springcore/service/ProductService.java
@@ -0,0 +1,49 @@
+package com.sparta.springcore.service;
+
+
+import com.sparta.springcore.dto.ProductMyPriceRequestDto;
+import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.model.Product;
+import com.sparta.springcore.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(ProductRequestDto requestDto) {
+// 요청받은 DTO 로 DB에 저장할 객체 만들기
+        Product product = new Product(requestDto);
+
+        productRepository.save(product);
+
+        return product;
+    }
+
+    public Product updateProduct(Long id, ProductMyPriceRequestDto requestDto) {
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
+        );
+
+        int myprice = requestDto.getMyprice();
+        product.setMyprice(myprice);
+        productRepository.save(product);
+
+        return product;
+    }
+
+    public List<Product> getProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products;
+    }
+}
